a8b39074770861a66f3120c3170982ab19b93eae
Yue0925 <mellet1997@outlook.com>
2022-03-11 17:43:28 +0100

    new results

--- Daniel/TP_NOTE/TPNOTE/ex.jl
+++ Daniel/TP_NOTE/TPNOTE/ex.jl
@@ -123,69 +123,73 @@ function cp_ex2()
     statusMP = termination_status(MP)
     @info "masterPB status $statusMP"
 
-    while (statusMP == MOI.DUAL_INFEASIBLE) || (statusMP == MOI.OPTIMAL)
-        if ite > MAXITE
+    function adding_cuts()
+        while (statusMP == MOI.DUAL_INFEASIBLE) || (statusMP == MOI.OPTIMAL)
+            if ite > MAXITE
+                optimize!(MP)
+                solved_Time = round(time() - start_time, digits = 2)
+                return (solved_Time, ite, round(Int, objective_value(MP)))
+            end
+    
+            ite += 1
+    
             optimize!(MP)
-            solved_Time = round(time() - start_time, digits = 2)
-            return (solved_Time, ite, round(Int, objective_value(MP)))
+            # status of model
+            statusMP = termination_status(MP)
+            @info "masterPB status $statusMP"
+            println("\n ---------------")
+            println(" ite : ", ite)
+            #@show value.(MP[:y])
+    
+            y_star = value.(MP[:y])
+            obj_v = objective_value(MP)
+            @show "objective_value(MP) = $obj_v"
+    
+            # ----------------
+            # step 2
+            # ----------------
+            SM = sub_model(y_star)
+            optimize!(SM)
+    
+            # status of model
+            statusSM = termination_status(SM)
+            @info "subproblem status $statusSM"
+            @show objective_value(SM)
+            # @show value.(SM[:v])
+    
+            # ------------------
+            # step 3
+            # ------------------
+            if termination_status(SM) == MOI.INFEASIBLE_OR_UNBOUNDED
+                error("shouldn't apprear here :(")
+            elseif TOL < objective_value(SM)
+                @info "feasibility cut added ! "
+                v_star = value.(SM[:v])
+                @constraint(MP, 0 >= -sum(v_star[i] * y[i] * bnd for i in 1:M) + sum(D[i-M] * v_star[i] for i in M+1:N+M))
+            else
+                println("Ending with subproblem objective = ", objective_value(SM), " total ite : ", ite)
+                break
+            end
+    
         end
-
-        ite += 1
-
-        optimize!(MP)
-        # status of model
-        statusMP = termination_status(MP)
-        @info "masterPB status $statusMP"
-        println("\n ---------------")
-        println(" ite : ", ite)
-        #@show value.(MP[:y])
-
-        y_star = value.(MP[:y])
-        obj_v = objective_value(MP)
-        @show "objective_value(MP) = $obj_v"
-
-        # ----------------
-        # step 2
-        # ----------------
-        SM = sub_model(y_star)
-        optimize!(SM)
-
-        # status of model
-        statusSM = termination_status(SM)
-        @info "subproblem status $statusSM"
-        @show objective_value(SM)
-        # @show value.(SM[:v])
-
-        # ------------------
-        # step 3
-        # ------------------
-        if termination_status(SM) == MOI.INFEASIBLE_OR_UNBOUNDED
-            error("shouldn't apprear here :(")
-        elseif TOL < objective_value(SM)
-            @info "feasibility cut added ! "
-            v_star = value.(SM[:v])
-            @constraint(MP, 0 >= -sum(v_star[i] * y[i] * bnd for i in 1:M) + sum(D[i-M] * v_star[i] for i in M+1:N+M))
-        else
-            println("Ending with subproblem objective = ", objective_value(SM), " total ite : ", ite)
-            break
-        end
-
     end
 
-    println("Finally objective = ", objective_value(MP))
-    # @show value.(MP[:y])
-
     set_integer.(MP[:y])
+    ite = 0
+    adding_cuts()
+
     optimize!(MP)
+
     statusMP = termination_status(MP)
     @info "masterPB status $statusMP"
     obj_v = objective_value(MP)
     @show "objective_value(MP) = $obj_v"
 
-
     solved_Time = round(time() - start_time, digits = 2)
     return (solved_Time, ite, round(Int, obj_v))
 end
+
+
 
 """
 For bande passante equals to 1 only !!
@@ -231,5 +235,4 @@ function Dijkstra()
     obj_v = sum(dist .* D)
     @show obj_v
     return (round(solveTime, digits = 2), round(Int, obj_v))
-
 end
